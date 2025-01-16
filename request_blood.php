01eab8622c37e6cc731b1e6440871fcdd1b9c738
DrgJrc <jayaram.chaulagain2043@gmail.com>
2025-01-16 12:14:19 +0545

    Perfectly working for single donor request 81/10/3 12:14

--- request_blood.php
+++ request_blood.php
@@ -154,4 +154,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit_request'])) {
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-</html>
+</html>   
